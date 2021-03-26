6eff5e7ec1ec24bc8d3ea4db467458deae36809f
ekwame001 <61211821+ekwame001@users.noreply.github.com>
2021-03-26 23:42:22 +0000

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -6,7 +6,29 @@
 #include <ctime>
 
 using namespace std;
+int LinearIndex(int i, int j){
+    if(j>1) return -1;
+    else{
+        int x =(((i*i)+i)/2)+j;
+        return x;
+    }
+}
 
+int* ReverseIdx(int arr[7][7], int x, int arr1[24]){
+    for(int i=0; i<7; i++){
+        for(int j=0; j<7; j++){
+            if(j<=i){
+                int temp = (((i*i)+i)/2)+j;
+                if(temp==x){
+                    arr[0] = i;
+                    arr[1] = j;
+                    return arr;
+                }
+            }
+        }
+    }
+    return arr;
+}
 int main()
 {
     int a=8;
